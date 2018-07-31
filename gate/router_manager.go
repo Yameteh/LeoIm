0a57832705cc081269ccdbd13dbe804caece3637
yameteh <ygj586@163.com>
2018-07-31 23:06:58 +0800

    add router

--- gate/router_manager.go
+++ gate/router_manager.go
@@ -1,8 +1,48 @@
 package main
 
+import (
+	"net/rpc"
+	"github.com/golang/glog"
+)
+
 type RouterManager struct {
+	RouterCount int
+	curIndex    int
+	lastIndex   int
 }
 
-func NewRouterManager(addr []string) *RouterManager {
-	return &RouterManager{}
+func NewRouterManager(count int) *RouterManager {
+	return &RouterManager{count, 0, -1}
 }
+
+func (rm *RouterManager) PublishMessage(p *Protocol) {
+	var client *rpc.Client
+	var err error
+	defer client.Close()
+	for {
+		client, err = rpc.DialHTTP("tcp", config.RouterServer[rm.curIndex])
+		if err == nil {
+			rm.lastIndex = rm.curIndex
+			message := &Message{}
+			message.Version = p.Version
+			message.Type = p.Type
+			message.Body = p.Body
+			var ret int
+			client.Call("RouterRpcServer.HandleMessage", message,&ret)
+			if ret != 0 {
+				glog.Error("router server publish response error ")
+			}
+		} else {
+			if rm.curIndex == rm.lastIndex {
+				glog.Error("no router server available when publish")
+				return
+			}
+		}
+		rm.curIndex ++
+		if rm.curIndex >= len(config.RouterServer) {
+			rm.curIndex = 0
+		}
+
+	}
+}
+
--- gate/rpc_params.go
+++ gate/rpc_params.go
@@ -4,3 +4,10 @@ type User struct {
 	Account  string
 	Password string
 }
+
+type Message struct {
+	Version uint8
+	Type    uint8
+	Body    string
+}
+
