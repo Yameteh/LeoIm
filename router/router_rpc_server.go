39e704e5e64719eebc25702d0f88ecb5f7bb5cc7
yao.guoju <yao.guoju@hoowe.cn>
2018-08-10 14:06:04 +0800

    修改router并发

--- router/router_rpc_server.go
+++ router/router_rpc_server.go
@@ -21,42 +21,45 @@ func (rrs *RouterRpcServer) saveMessage(body *MessageBody) bool {
 		glog.Error(err)
 		return false
 	}
-	glog.Info("router save message ",body)
+	glog.Info("router save message ", body)
 	return true
 }
 
 func (rrs *RouterRpcServer) HandleMessage(msg *Message, ret *int) error {
-	switch msg.Type {
-	case 2:
-		m := new(MessageBody)
-		err := json.Unmarshal([]byte(msg.Body), m)
-		if err != nil {
-			return err
-		}
-		if rrs.saveMessage(m) {
-			tp := new(ToProtocol)
-			tp.To = m.To
-			tp.Version = msg.Version
-			tp.Type = 4
-			sr := &SyncResponse{}
-			sr.Time = m.Time
-			sr.Server = fmt.Sprintf("%s:%d",config.WebDomain,config.WebPort);
-			s,_ := json.Marshal(sr)
-			tp.Body = string(s)
-			tp.Length = uint32(len(tp.Body))
-			gateManager.PublishProtocol(tp)
-		}else {
-			glog.Info("router save message failed ")
-			tp := new(ToProtocol)
-			tp.To = m.To
-			tp.Version = msg.Version
-			tp.Type = msg.Type
-			tp.Body = msg.Body
-			tp.Length = uint32(len(tp.Body))
-			glog.Info("router transfer message ",tp)
-			gateManager.PublishProtocol(tp)
-		}
+	go func(msg *Message) {
+		switch msg.Type {
+		case 2:
+			m := new(MessageBody)
+			err := json.Unmarshal([]byte(msg.Body), m)
+			if err != nil {
+				glog.Error(err)
+				return;
+			}
+			if rrs.saveMessage(m) {
+				tp := new(ToProtocol)
+				tp.To = m.To
+				tp.Version = msg.Version
+				tp.Type = 4
+				sr := &SyncResponse{}
+				sr.Time = m.Time
+				sr.Server = fmt.Sprintf("%s:%d", config.WebDomain, config.WebPort);
+				s, _ := json.Marshal(sr)
+				tp.Body = string(s)
+				tp.Length = uint32(len(tp.Body))
+				gateManager.PublishProtocol(tp)
+			} else {
+				glog.Info("router save message failed ")
+				tp := new(ToProtocol)
+				tp.To = m.To
+				tp.Version = msg.Version
+				tp.Type = msg.Type
+				tp.Body = msg.Body
+				tp.Length = uint32(len(tp.Body))
+				glog.Info("router transfer message ", tp)
+				gateManager.PublishProtocol(tp)
+			}
 
-	}
+		}
+	}(msg)
 	return nil
 }
